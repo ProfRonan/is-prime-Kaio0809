343f08ad9ee5c7bfc65a15349aa03342319f9877
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-04-08 10:45:15 +0000

    exercicio concluido

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ numero = int(input('Digite um número inteiro: '))
 if numero < 0 or numero == 0:
     print('Número inválido')
 else:
-    for primos in range(1,numero + 1 ):
+    for primos in range(1,numero+1):
         if (numero % primos) == 0:
             cont += 1
-    if cont > 2:
+    if cont > 2 or numero == 1:
         print ('Não primo')
     else: 
         print('Primo')
